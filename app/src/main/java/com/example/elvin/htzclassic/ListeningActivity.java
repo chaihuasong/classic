ba89f8a26a5254d12d398d980e28f94b09f2448a
elvin du <nick@yunio.com>
2017-09-18 18:43:08 +0800

    update

--- app/src/main/java/com/example/elvin/htzclassic/ListeningActivity.java
+++ app/src/main/java/com/example/elvin/htzclassic/ListeningActivity.java
@@ -1,11 +1,30 @@
 package com.example.elvin.htzclassic;
 
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.ServiceConnection;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ImageButton;
+
+import com.example.elvin.htzclassic.view.ILrcBuilder;
+import com.example.elvin.htzclassic.view.ILrcView;
+import com.example.elvin.htzclassic.view.impl.DefaultLrcBuilder;
+import com.example.elvin.htzclassic.view.impl.LrcRow;
+import com.example.elvin.htzclassic.view.impl.LrcView;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.List;
+
 
 public class ListeningActivity extends AppCompatActivity {
     private ImageButton mImageButtonPlay;
@@ -14,6 +33,11 @@ public class ListeningActivity extends AppCompatActivity {
     public final  static  int PLAY = 1;
     public final  static  int PAUSE= 2;
     public final  static  int STOP = 3;
+
+    private ILrcView mLrcView;
+    private MsgReceiver msgReceiver;
+
+    public final  static String   SERVICE_PLAYING_ACTION = "com.example.elvin.htzclassic.service.playing";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +49,23 @@ public class ListeningActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         init();
+
+        //从assets目录下读取歌词文件内容
+        String lrc = getFromAssets("nan_er_dang_zi_qiang.lrc");
+        mLrcView=(ILrcView)findViewById(R.id.lrcView);
+
+        //解析歌词构造器
+        ILrcBuilder builder = new DefaultLrcBuilder();
+        //解析歌词返回LrcRow集合
+        List<LrcRow> rows = builder.getLrcRows(lrc);
+        //将得到的歌词集合传给mLrcView用来展示
+        mLrcView.setLrc(rows);
+
+        msgReceiver = new MsgReceiver();
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(SERVICE_PLAYING_ACTION);
+        registerReceiver(msgReceiver,intentFilter);
+
     }
 
     private  void playMusic(int type){
@@ -51,4 +92,33 @@ public class ListeningActivity extends AppCompatActivity {
             }
         });
     }
+
+    public String getFromAssets(String fileName){
+        try{
+            InputStreamReader inputStreamReader = new InputStreamReader(getResources().getAssets().open(fileName));
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            String line = "";
+            String result = "";
+            while ((line = bufferedReader.readLine()) != null){
+                if(line.trim().equals(""))
+                    continue;
+
+                result += line + "\r\n";
+            }
+
+            return  result;
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return  "";
+    }
+
+   public class  MsgReceiver extends BroadcastReceiver{
+       @Override
+       public void onReceive(Context context, Intent intent) {
+           int position = intent.getIntExtra("position",0);
+           mLrcView.seekLrcToTime(position);
+       }
+   }
 }
--- app/src/main/java/com/example/elvin/htzclassic/PlayingMusicService.java
+++ app/src/main/java/com/example/elvin/htzclassic/PlayingMusicService.java
@@ -2,19 +2,35 @@ package com.example.elvin.htzclassic;
 
 import android.app.Service;
 import android.content.Intent;
+import android.content.pm.ProviderInfo;
+import android.media.AudioRecord;
 import android.media.MediaPlayer;
+import android.os.Binder;
 import android.os.IBinder;
 import android.support.annotation.IntDef;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class PlayingMusicService extends Service {
+    private  final  static  String TAG = "PlayingMusicService";
     private MediaPlayer mediaPlayer;
     private Boolean isStop = true;
+
+    private Timer timer;
+    private LrcTask task;
 
     @Override
     public void onCreate() {
         super.onCreate();
+        timer = new Timer();
+
         if (null == mediaPlayer){
             mediaPlayer = new MediaPlayer();
+
             mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener(){
                 @Override
                 public void onCompletion(MediaPlayer mp){
@@ -23,6 +39,37 @@ public class PlayingMusicService extends Service {
                     sendBroadcast(intent);
                 }
             });
+
+            mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                @Override
+                public void onPrepared(MediaPlayer mediaPlayer) {
+                    Log.d("dd","dd");
+//                    mediaPlayer.start();
+                    timer.schedule(task,0,10000);
+                }
+            });
+
+            mediaPlayer.setOnErrorListener(new MediaPlayer.OnErrorListener() {
+                @Override
+                public boolean onError(MediaPlayer mediaPlayer, int i, int i1) {
+                    Log.e(TAG,""+i +""+i1);
+                    return false;
+                }
+            });
+        }
+    }
+
+    class LrcTask extends  TimerTask{
+        @Override
+        public void run() {
+            Log.d("dd","ddfsafas");
+//            if(mediaPlayer.isPlaying()){
+//                Intent intent = new Intent(ListeningActivity.SERVICE_PLAYING_ACTION);
+//                int position = mediaPlayer.getCurrentPosition();
+//                intent.putExtra("position",position);
+//                sendBroadcast(intent);
+//            }
+
         }
     }
 
@@ -34,6 +81,7 @@ public class PlayingMusicService extends Service {
                     mediaPlayer.reset();
                     mediaPlayer = MediaPlayer.create(this,R.raw.music);
                     mediaPlayer.start();
+                    Log.e(TAG,""+mediaPlayer.getCurrentPosition());
                     mediaPlayer.setLooping(false);
                     isStop = false;
                 }else if(!isStop && mediaPlayer != null && !mediaPlayer.isPlaying() ){
@@ -62,6 +110,6 @@ public class PlayingMusicService extends Service {
     @Override
     public IBinder onBind(Intent intent) {
         // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        return  null;
     }
 }
